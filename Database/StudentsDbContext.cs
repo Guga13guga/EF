1f49aef3c6bd5b3345055c8390a4c9760a4445cd
STEP <step@gmail.com>
2025-11-03 20:32:39 +0400

    Add project files.

---
+++ Database/StudentsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF.Database;
+
+public class StudentsDbContext:DbContext
+{
+    public StudentsDbContext():base()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=StudentsStorage;Integrated Security=True");
+    }
+}
